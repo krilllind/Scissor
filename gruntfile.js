d971d373cd44b85042a80422153ef4e6e462ddef
Kristoffer <krillllind@gmail.com>
2017-01-13 11:40:30 +0100

    Initial push

---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+﻿/*
+** Global module
+*/
+
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: { '/src/scissor.css': '/src/utils/*.scss' }
+            }
+        },
+        uglify: {
+            options: {
+                sourceMap: true
+            },
+            core: { files: { '/src/scissor.min.js': ['/src/utils/*.js'] } }
+        },
+        watch: {
+            assets: { files: ['/src/utils/**/*.scss'], tasks: ['sass'] },
+            scripts: { files: ['/src/utils/**/*.js'], tasks: ['uglify'] }
+        }
+    });
+
+    grunt.registerTask('watch', ['watch']);
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-sass');
+};
